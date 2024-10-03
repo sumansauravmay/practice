091094f6875545d83be20aecc2b4ed4b498d4fda
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-03 17:37:04 +0530

    find unique

--- reactone/src/App.js
+++ reactone/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  // let arr=["Suman", "Saurav", "Suman Saurav", "Suman"];
+  // const [data, setData]=React.useState([...new Set(arr)])
+
+  // let arr = [
+  //   { id: 1, name: "Suman" },
+  //   { id: 2, name: "Saurav" },
+  //   { id: 3, name: "Suman Saurav" },
+  //   { id: 4, name: "Suman" }
+  // ];
+
+  const [data, setData] = React.useState([
+    { id: 1, name: "Suman" },
+    { id: 2, name: "Saurav" },
+    { id: 3, name: "Suman Saurav" },
+    { id: 4, name: "Suman" },
+  ]);
+  console.log(data);
+
+  const handleDelete = (id) => {
+    let data2 = data.filter((item) => {
+      return item.id !== id;
+    });
+    setData(data2);
+  };
+
+  let unique = new Set();
+  let uniquedata = data.filter((el) => {
+    if (!unique.has(el.name)) {
+      unique.add(el.name);
+      return true;
+    }
+    return false;
+  });
+  console.log("uniquedata", uniquedata);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {uniquedata.map((item) => (
+        <div key={item.id} onClick={() => handleDelete(item.id)}>
+          {item.name}
+        </div>
+      ))}
     </div>
   );
 }
 
 export default App;
+
+
